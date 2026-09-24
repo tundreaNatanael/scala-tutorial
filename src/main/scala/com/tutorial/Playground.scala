30334cc50d515dfd2730003342bd0358f84dda57
nathan <natanael.tundrea@clearviewsocial.com>
2026-09-24 22:35:13 +0300

    play: anonymous functions

--- src/main/scala/com/tutorial/Playground.scala
+++ src/main/scala/com/tutorial/Playground.scala
@@ -1,8 +1,14 @@
 package com.tutorial
 
-def add(x:Int, y:Double):Double = {
-   return x + y
-}
+def add1(x:Int, y:Int) = x + y //method
+
 
 @main def start(): Unit =
-  println(add(42,13.04))
+  val add2 = (x:Int, y:Int) => x + y //anonymous function
+  val add3:(Int,Int)=>Int = _ + _ //alternate way
+  val add4 = (_ + _):((Int,Int)=>Int) //alternate way, rare
+  
+  println(add1(42,13))
+  println(add2(42,13))
+  println(add3(42,13))
+  println(add4(42,13))
