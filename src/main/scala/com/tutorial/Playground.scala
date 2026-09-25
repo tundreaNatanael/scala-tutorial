fc772354fee5c1eb54d5d085eeebbb2dec141ec1
nathan <natanael.tundrea@clearviewsocial.com>
2026-09-25 10:31:01 +0300

    play: assign multiple variables

--- src/main/scala/com/tutorial/Playground.scala
+++ src/main/scala/com/tutorial/Playground.scala
@@ -4,11 +4,5 @@ def add1(x:Int, y:Int) = x + y //method
 
 
 @main def start(): Unit =
-  val add2 = (x:Int, y:Int) => x + y //anonymous function
-  val add3:(Int,Int)=>Int = _ + _ //alternate way
-  val add4 = (_ + _):((Int,Int)=>Int) //alternate way, rare
-  
-  println(add1(42,13))
-  println(add2(42,13))
-  println(add3(42,13))
-  println(add4(42,13))
+  var (x, y, z, c, python, java) = ("1", 2, 3, true, false, "no!")
+  println((x, y, z, c, python, java))
